e1f7f368891b009f986761428c89c27ed385988f
Jan-Jaap van Gool <jan.jaap.v.g@gmail.com>
2022-05-11 23:45:47 +0200

    Changed Header
    Added social icons package, Added social icons to header, changed header styling

--- portfolio/src/Components/HeaderComponent/Header.js
+++ portfolio/src/Components/HeaderComponent/Header.js
@@ -1,37 +1,66 @@
 import "./Header.css";
+import { SocialIcon } from "react-social-icons";
 
 export default function Header() {
   return (
     <div className="header">
-      <div id="title">
-        <h1 className="title">Portfolio</h1>
-      </div>
+      <div className="header-container">
+        <div id="title">
+          <h1 className="title">Portfolio</h1>
+        </div>
 
-      <div className="links">
-        <div className="link">
-          <a href="/" className="navLink">
-            About
-          </a>
+        <div className="links">
+          <div className="link">
+            <a href="/" className="navLink">
+              About
+            </a>
+          </div>
+          <div className="link">
+            <a href="/" className="navLink">
+              Projects
+            </a>
+          </div>
+          <div className="link">
+            <a href="/" className="navLink">
+              Skills
+            </a>
+          </div>
+          <div className="link">
+            <a href="/" className="navLink">
+              Education
+            </a>
+          </div>
+          <div className="link">
+            <a href="/" className="navLink">
+              Contact
+            </a>
+          </div>
         </div>
-        <div className="link">
-          <a href="/" className="navLink">
-            Projects
-          </a>
-        </div>
-        <div className="link">
-          <a href="/" className="navLink">
-            Skills
-          </a>
-        </div>
-        <div className="link">
-          <a href="/" className="navLink">
-            Education
-          </a>
-        </div>
-        <div className="link">
-          <a href="/" className="navLink">
-            Contact
-          </a>
+
+        <div className="socials">
+          <div className="social">
+            <SocialIcon
+              url="https://www.linkedin.com/in/jan-jaap-van-gool-00a382171/"
+              fgColor="white"
+            />
+          </div>
+
+          <div className="social">
+            <SocialIcon
+              url="https://github.com/JanJaapvG"
+              bgColor="black"
+              fgColor="white"
+            />
+          </div>
+
+          <div className="social">
+            <SocialIcon
+              url="mailto:jan.jaap.v.g@gmail.com"
+              network="email"
+              bgColor="orange"
+              fgColor="white"
+            />
+          </div>
         </div>
       </div>
     </div>
